c5031b00cb7b33fa90af0fec903018282f035cbd
jatin831 <jatinbajaj2001@gmail.com>
2021-05-30 06:39:17 +0530

    video carousel

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var owl = $('.owl-carousel').owlCarousel({
+var owl = $('.owl-one').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
@@ -10,7 +10,7 @@ var owl = $('.owl-carousel').owlCarousel({
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
             items:3
@@ -24,4 +24,52 @@ $('.customNextBtn').click(function() {
 
 $('.customPrevBtn').click(function() {
     owl.trigger('prev.owl.carousel');
+})
+
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+var owl2 = $('.owl-two').owlCarousel({
+    items:1,
+    merge:true,
+    loop:true,
+    margin: 10,
+    video:true,
+    center: true,
+    lazyLoad:false,
+    videoHeight: 400,
+    videoWidth: '100%',
+    dots: false,
+    responsive:{
+        480:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
+
+$('.videoPrev').click(function() {
+    owl2.trigger('prev.owl.carousel');
+})
+
+$('.videoNext').click(function() {
+    owl2.trigger('next.owl.carousel');
 })
